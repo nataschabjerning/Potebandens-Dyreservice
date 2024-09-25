836a69a5c86d98527f953e9fb845bd1fdf8bff08
“nataschabjerning” <ngb@personalezonen.dk>
2024-09-25 09:01:25 +0200

    added overview of database tables

--- server.php
+++ server.php
@@ -1,13 +1,46 @@
-<!-- home
+<!-- 
+
+# HOME DATABASE
 
 $serverName = "localhost";
 $dbName = "potebandens_dyreservice_db";
 $dbusername = "root";
 $dbpassword = "";
 
-work
+# LAPTOP DATABASE
+
+$serverName = "localhost";
+---- CHECK THIS ----
+$dbName = "potebandens_dyreservice_db";
+$dbusername = "root";
+$dbpassword = "";
+
+# WORK DATABASE
 
 $serverName = "local.potebandens-dyreservice.dk";
 $dbName = "potebandens_dyreservice_db";
 $dbusername = "root";
-$dbpassword = "root"; -->
+$dbpassword = "root";
+
+# DATABASE - TABLES
+
+gallery
+- id
+- image_link
+- image_alt
+- image_text
+
+services
+- id
+- service_name
+- service_length
+- service_description
+
+users
+- id
+- name
+- username
+- email
+- password
+
+-->
