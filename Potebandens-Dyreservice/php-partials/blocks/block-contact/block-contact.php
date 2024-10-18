379131a802e2f4ff68f671522d30ea224ffbfc3f
“nataschabjerning” <ngb@personalezonen.dk>
2024-10-18 12:41:45 +0200

    added info and styling on block-contact.php

--- Potebandens-Dyreservice/php-partials/blocks/block-contact/block-contact.php
+++ Potebandens-Dyreservice/php-partials/blocks/block-contact/block-contact.php
@@ -1,3 +1,12 @@
+<?php
+    include("includes/connect.inc.php");
+
+    $sql = "SELECT * FROM contact;";
+    $stmt = $conn->prepare($sql);
+    mysqli_stmt_execute($stmt);
+    $resultData = mysqli_stmt_get_result($stmt);
+?>
+
 <div class="block contact">
 
     <div class="container">
@@ -11,54 +20,96 @@
         <br>
         <br>
         <hr>
-        <br>
-        <br>
+
+        <div class="phone-time">
+            <h4>Telefontider</h4>
+            <div class="time">
+                <div class="day">
+                    <p>Mandag:</p>
+                    <span>9:00 - 15:00</span>
+                </div>
+                <div class="day">
+                    <p>Tirsdag:</p>
+                    <span>9:00 - 15:00</span>
+                </div>
+                <div class="day">
+                    <p>Onsdag:</p>
+                    <span>9:00 - 15:00</span>
+                </div>
+                <div class="day">
+                    <p>Torsdag:</p>
+                    <span>9:00 - 15:00</span>
+                </div>
+                <div class="day">
+                    <p>Fredag:</p>
+                    <span>9:00 - 15:00</span>
+                </div>
+                <div class="day">
+                    <p>Lørdag:</p>
+                    <span>Lukket</span>
+                </div>
+                <div class="day">
+                    <p>Søndag:</p>
+                    <span>Lukket</span>
+                </div>
+            </div>
+        </div>
 
         <h3>loop igennem info, som hentes fra contact table og lav ny boks pr. person</h3>
 
-        <div class="phone-time">
-            <h3>Telefontid</h3>
-            <div class="time">
-                <p>Mandag: 9:00 - 15:00</p>
-                <p>Tirsdag: 9:00 - 15:00</p>
-                <p>Onsdag: 9:00 - 15:00</p>
-                <p>Torsdag: 9:00 - 15:00</p>
-                <p>Fredag: 9:00 - 15:00</p>
-                <p>Lørdag: Lukket</p>
-                <p>Søndag: Lukket</p>
-            </div>
-        </div>
-        <div class="contact-wrapper">
-            <div class="contact-options">
-                <div class="name">
-                    <h2>Lonni A. Danielsen</h2>
-                </div>
-                <div class="work_title">
-                    <p>Dyreadfærdsterapeut</p>
-                </div>
-                <div class="phone">
-                    <div class="number">
-                        <h3>Mobil</h3>
-                        <p>53541944</p>
-                    </div>
-                </div>
-                <div class="email">
-                    <h3>Email</h3>
-                    <!-- <p>lonni1964@gmail.com</p> -->
-                    <a href="lonni1964@gmail.com">lonni1964@gmail.com</a>
-                </div>
-                <div class="adress">
-                    <h3>Adresse</h3>
-                    <p>Solkær 34, St. Th.</p>
-                    <p>2605 Brøndby</p>
-                </div>
-            </div>
-        </div> <!-- .contact-wrapper end -->
 
         <br>
         <br>
-        <h3>Lav form til at sende besked til admin indbakke</h3>
+
+        <div class="contact-blocks">
+
+            <?php while($row = mysqli_fetch_assoc($resultData)) { ?>
+                <div class="contact-wrapper">
+                    <div class="contact-options">
+                        
+                        <?php if (!empty($row['name'])) { ?>
+                            <div class="name">
+                                <h2><?php echo $row['name']?></h2>
+                            </div>
+                        <?php } ?>
+
+                        <?php if (!empty($row['work_title'])) { ?>
+                            <div class="work_title">
+                                <h3>- <?php echo $row['work_title']?></h3>
+                            </div>
+                        <?php } ?>
+
+                        <div class="phone_mail">
+                            <?php if (!empty($row['phone'])) { ?>
+                                <div class="phone">
+                                    <div class="number">
+                                        <h4>Mobil</h4>
+                                        <a href="tel:<?php echo $row['phone']?>"><?php echo $row['phone']?></a>
+                                    </div>
+                                </div>
+                            <?php } ?>
+                            
+                            <?php if (!empty($row['email'])) { ?>
+                                <div class="email">
+                                    <h4>Email</h4>
+                                    <a href="mailto:<?php echo $row['email']?>"><?php echo $row['email']?></a>
+                                </div>
+                            <?php } ?>
+                        </div>
+
+                    </div> <!-- .contact-options end -->
+                </div> <!-- .contact-wrapper end -->
+            <?php } ?>
+
+        </div>
+
         <br>
+        <br>
+
+        <h3>Lav form til at sende besked til admin indbakke</h3>
+        
+        <br>
+        
         navn <br>
         emne <br>
         besked <br>
@@ -66,6 +117,14 @@
         indtast mobilnummer eller email, alt efter hvilken du har krydset af
         send <br>
 
+        <br>
+
+        if selected phone is not empty and input phone is empty
+        - du har glemt at give os dit telefonnummer
+        if selected email is not empty and input email is empty
+        - du har glemt at give os din email
+
+        <br>
         <br>
 
         <h3>Besked bliver uploaded til inbox table i database og admin kan se den i indbakke, som kan findes på 'profil.php' (list.php)</h3>
