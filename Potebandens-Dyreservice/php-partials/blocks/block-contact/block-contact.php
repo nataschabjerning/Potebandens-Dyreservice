691091eecd0478b782440ce2c9a32015bebc7e05
“nataschabjerning” <ngb@personalezonen.dk>
2024-10-18 13:56:57 +0200

    slowly adding form on block-contact.php to send message to admin

--- Potebandens-Dyreservice/php-partials/blocks/block-contact/block-contact.php
+++ Potebandens-Dyreservice/php-partials/blocks/block-contact/block-contact.php
@@ -11,81 +11,36 @@
 
     <div class="container">
 
-        <h1>styling og formulering til denne info</h1>
-        <h2>Hvordan kan du kontakte os?</h2>
-        <h3>- Skriv en email</h3>
-        <h3>- Ring eller send en sms til telefonnummer</h3>
-        <h3>- Udfyld formen nedenfor og vi vender tilbage snarest</h3>
+        <h1>Hvordan kan du kontakte os?</h1>
 
-        <br>
-        <br>
-        <hr>
+        <div class="contact-info">
 
-        <div class="phone-time">
-            <h4>Telefontider</h4>
-            <div class="time">
-                <div class="day">
-                    <p>Mandag:</p>
-                    <span>9:00 - 15:00</span>
-                </div>
-                <div class="day">
-                    <p>Tirsdag:</p>
-                    <span>9:00 - 15:00</span>
-                </div>
-                <div class="day">
-                    <p>Onsdag:</p>
-                    <span>9:00 - 15:00</span>
-                </div>
-                <div class="day">
-                    <p>Torsdag:</p>
-                    <span>9:00 - 15:00</span>
-                </div>
-                <div class="day">
-                    <p>Fredag:</p>
-                    <span>9:00 - 15:00</span>
-                </div>
-                <div class="day">
-                    <p>Lørdag:</p>
-                    <span>Lukket</span>
-                </div>
-                <div class="day">
-                    <p>Søndag:</p>
-                    <span>Lukket</span>
-                </div>
+            <div class="contact-title">
+                <h2>Ring eller send en SMS</h2>
+                <h2>Skriv en email</h2>
             </div>
-        </div>
+            
+            <div class="contact-blocks">
+                <?php while($row = mysqli_fetch_assoc($resultData)) { ?>
+                    <div class="contact-wrapper">
+                        <div class="contact-options">
+                            
+                            <?php if (!empty($row['name'])) { ?>
+                                <div class="name">
+                                    <h2><?php echo $row['name']?></h2>
+                                </div>
+                            <?php } ?>
 
-        <h3>loop igennem info, som hentes fra contact table og lav ny boks pr. person</h3>
+                            <?php if (!empty($row['work_title'])) { ?>
+                                <div class="work_title">
+                                    <h3><?php echo $row['work_title']?></h3>
+                                </div>
+                            <?php } ?>
 
-
-        <br>
-        <br>
-
-        <div class="contact-blocks">
-
-            <?php while($row = mysqli_fetch_assoc($resultData)) { ?>
-                <div class="contact-wrapper">
-                    <div class="contact-options">
-                        
-                        <?php if (!empty($row['name'])) { ?>
-                            <div class="name">
-                                <h2><?php echo $row['name']?></h2>
-                            </div>
-                        <?php } ?>
-
-                        <?php if (!empty($row['work_title'])) { ?>
-                            <div class="work_title">
-                                <h3>- <?php echo $row['work_title']?></h3>
-                            </div>
-                        <?php } ?>
-
-                        <div class="phone_mail">
                             <?php if (!empty($row['phone'])) { ?>
                                 <div class="phone">
-                                    <div class="number">
-                                        <h4>Mobil</h4>
-                                        <a href="tel:<?php echo $row['phone']?>"><?php echo $row['phone']?></a>
-                                    </div>
+                                    <h4>Mobil</h4>
+                                    <a href="tel:<?php echo $row['phone']?>"><?php echo $row['phone']?></a>
                                 </div>
                             <?php } ?>
                             
@@ -95,16 +50,107 @@
                                     <a href="mailto:<?php echo $row['email']?>"><?php echo $row['email']?></a>
                                 </div>
                             <?php } ?>
+
+                        </div> <!-- .contact-options end -->
+                    </div> <!-- .contact-wrapper end -->
+                <?php } ?>
+
+            </div> <!-- .contact-blocks end -->
+
+            <div class="phone-time">
+                <h4>Telefontider</h4>
+                <div class="time">
+                    <div class="day">
+                        <p>Mandag:</p>
+                        <span>9:00 - 15:00</span>
+                    </div>
+                    <div class="day">
+                        <p>Tirsdag:</p>
+                        <span>9:00 - 15:00</span>
+                    </div>
+                    <div class="day">
+                        <p>Onsdag:</p>
+                        <span>9:00 - 15:00</span>
+                    </div>
+                    <div class="day">
+                        <p>Torsdag:</p>
+                        <span>9:00 - 15:00</span>
+                    </div>
+                    <div class="day">
+                        <p>Fredag:</p>
+                        <span>9:00 - 15:00</span>
+                    </div>
+                    <div class="day">
+                        <p>Lørdag:</p>
+                        <span>Lukket</span>
+                    </div>
+                    <div class="day">
+                        <p>Søndag:</p>
+                        <span>Lukket</span>
+                    </div>
+                </div> <!-- .time end -->
+            </div> <!-- .phone-time end -->
+        </div> <!-- .contact-info end -->
+
+        <div class="message-form">
+            
+            <div class="message-title">
+                <h2>Udfyld formen nedenfor og vi vender tilbage snarest</h2>
+            </div>
+
+            <div id="new-message">
+                <div class="new-message">
+                    
+                    <p class="span">* SKAL udfyldes</p>
+                    
+                    <form action="">
+                        
+                        <div class="message-name">
+                            <label>Dit Navn</label>
+                            <div class="input">
+                                <input type="text" name="message-name">
+                            </div>
                         </div>
 
-                    </div> <!-- .contact-options end -->
-                </div> <!-- .contact-wrapper end -->
-            <?php } ?>
+                        <div class="message-subject">
+                            <label>Emne</label>
+                            <div class="input">
+                                <input type="text" name="message-subject">
+                            </div>
+                        </div>
 
-        </div>
+                        <div class="message-msg">
+                            <label>Din Besked</label>
+                            <div class="input">
+                                <textarea name="message-msg" id="message-msg"></textarea>
+                            </div>
+                        </div>
 
-        <br>
-        <br>
+                        <select name="message-select" id="message-select">
+                            <option value="call">Opkald</option>
+                            <option value="sms">SMS</option>
+                            <option value="email">Email</option>
+                        </select>
+
+                        <div class="message-choice">
+                            <label>Mobil eller Email</label>
+                            <div class="input">
+                                <input type="text" name="message-choice">
+                            </div>
+                        </div>
+
+                    </form>
+
+                    <div class="button">
+                        <button id="send-message">Send Besked</button>
+                    </div>
+                </div>
+            </div>
+            
+            
+        </div> <!-- .message-form end -->
+
+        
 
         <h3>Lav form til at sende besked til admin indbakke</h3>
         
