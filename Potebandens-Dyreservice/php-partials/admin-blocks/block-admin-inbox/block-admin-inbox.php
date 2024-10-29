928aa97226b2dc987b948339f8000beace874dd9
“nataschabjerning” <ngb@personalezonen.dk>
2024-10-29 13:53:20 +0100

    a bit more styling for inbox

--- Potebandens-Dyreservice/php-partials/admin-blocks/block-admin-inbox/block-admin-inbox.php
+++ Potebandens-Dyreservice/php-partials/admin-blocks/block-admin-inbox/block-admin-inbox.php
@@ -17,28 +17,60 @@
             
             <div class="inbox-messages">
                 
-            <div class="table">
-                <div class="thead">
-                    <div class="th">Emne</div>
-                    <div class="th">Afsender</div>
-                </div>
-                <?php while($row = mysqli_fetch_assoc($resultData)) { ?>
-                <div class="tbody message-from">
-                    <div class="td"><?php echo $row['message_subject']?></div>
-                    <div class="td"><?php echo $row['message_name']?></div>
-                </div>
-                <div class="message-msg">
-                    <div class="message-top">
-                        <div class="td"><?php echo $row['message_contact']?></div>
-                        <div class="td"><?php echo $row['message_phone']?></div>
-                        <div class="td"><?php echo $row['message_email']?></div>
+                <div class="table">
+                    <div class="thead">
+                        <div class="th">Emne</div>
+                        <div class="th">Afsender</div>
                     </div>
-                    <div class="message-bottom">
-                        <div class="td"><?php echo $row['message_msg']?></div>
+                    <?php while($row = mysqli_fetch_assoc($resultData)) { ?>
+                    <div class="tbody message-from">
+                        <div class="td">
+                            <?php if(!empty($row['message_subject'])) { ?>
+                                <h4><?php echo $row['message_subject']?></h4>
+                            <?php } ?>
+                        </div>
+                        <div class="td">
+                            <?php if(!empty($row['message_name'])) { ?>
+                                <h4><?php echo $row['message_name']?></h4>
+                            <?php } ?>
+                        </div>
                     </div>
+                    <div class="message-msg">
+                        <div class="message-top">
+                            
+                            <div class="td message_contact">
+                                <h5>Kontakt</h5>
+                                <div class="how_to_contact">
+                                    <?php if(!empty($row['message_contact'])) { ?>
+                                        <h4><?php echo $row['message_contact']?></h4>
+                                    <?php } ?>
+                                </div>
+                            </div>
+
+                            <div class="td message_contact_method">
+                                <h5>Telefon / Email</h5>
+                                <div class="message_phone">
+                                    <?php if(!empty($row['message_phone'])) { ?>
+                                        <h4><?php echo $row['message_phone']?></h4>
+                                    <?php } ?>
+                                </div>
+                                <div class="message_email">
+                                    <?php if(!empty($row['message_email'])) { ?>
+                                        <a href="mailto:<?php echo $row['message_email']?>"><?php echo $row['message_email']?></a>
+                                    <?php } ?>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="message-bottom">
+                            <div class="td msg">
+                                <?php if(!empty($row['message_msg'])) { ?>
+                                    <p><?php echo $row['message_msg']?></p>
+                                <?php } ?>
+                            </div>
+                        </div>
+                    </div>
+                    <?php } ?>
                 </div>
-                <?php } ?>
-            </div>
                 
 
 
