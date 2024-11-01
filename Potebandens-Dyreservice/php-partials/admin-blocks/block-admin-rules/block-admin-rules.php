f6a6383595a2ee77611197ffd382f8232d14be08
“nataschabjerning” <ngb@personalezonen.dk>
2024-11-01 13:57:28 +0100

    adding a tiny bit of styling to admin-rules displayed from db

--- Potebandens-Dyreservice/php-partials/admin-blocks/block-admin-rules/block-admin-rules.php
+++ Potebandens-Dyreservice/php-partials/admin-blocks/block-admin-rules/block-admin-rules.php
@@ -25,37 +25,47 @@
                             <h4>ID:</h4>
                             <h4><?php echo $row['id']?></h4>
                         </div>
-                        <div class="rules">
-                            <h4><?php echo $row['rules']?></h4>
+                        <div class="rule">
+                            <input type="text" class="rules" value="<?php echo $row['rules']?>">
                             <div class="bullet-points">
                                 <div class="point">
+                                    <p>1)</p>
                                     <input type="text" class="point_one" value="<?php echo $row['rules_point_one']?>">
                                 </div>
                                 <div class="point">
+                                    <p>2)</p>
                                     <input type="text" class="point_two" value="<?php echo $row['rules_point_two']?>">
                                 </div>
                                 <div class="point">
+                                    <p>3)</p>
                                     <input type="text" class="point_three" value="<?php echo $row['rules_point_three']?>">
                                 </div>
                                 <div class="point">
+                                    <p>4)</p>
                                     <input type="text" class="point_four" value="<?php echo $row['rules_point_four']?>">
                                 </div>
                                 <div class="point">
+                                    <p>5)</p>
                                     <input type="text" class="point_five" value="<?php echo $row['rules_point_five']?>">
                                 </div>
                                 <div class="point">
+                                    <p>6)</p>
                                     <input type="text" class="point_six" value="<?php echo $row['rules_point_six']?>">
                                 </div>
                                 <div class="point">
+                                    <p>7)</p>
                                     <input type="text" class="point_seven" value="<?php echo $row['rules_point_seven']?>">
                                 </div>
                                 <div class="point">
+                                    <p>8)</p>
                                     <input type="text" class="point_eight" value="<?php echo $row['rules_point_eight']?>">
                                 </div>
                                 <div class="point">
+                                    <p>9)</p>
                                     <input type="text" class="point_nine" value="<?php echo $row['rules_point_nine']?>">
                                 </div>
                                 <div class="point">
+                                    <p>10)</p>
                                     <input type="text" class="point_ten" value="<?php echo $row['rules_point_ten']?>">
                                 </div>
                             </div>
